447ec76f4682d95d961c9186b776e7cf43d3738f
Dalatar <dalatarion@gmail.com>
2019-04-25 19:04:30 +0300

    add factory method

---
+++ Factory/Models/Sword.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory.Models
+{
+    class Sword: Weapon
+    {
+        public Sword()
+        {
+            Console.WriteLine("new sword forged");
+        }
+    }
+}
---
+++ Factory/Models/SwordMaster.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory.Models
+{
+    class SwordMaster:Blacksmith
+    {
+        public override Weapon Forge()
+        {
+            return new Sword();
+        }
+    }
+}
